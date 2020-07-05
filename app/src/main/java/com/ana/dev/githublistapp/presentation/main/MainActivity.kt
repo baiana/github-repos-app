9e8a7a62cabccecc5b2831e269b2d85ab013c17d
ana Dias <ana.luisa@cubos.io>
2020-07-05 18:04:44 -0300

    feat: add base layout for list item and search

--- app/src/main/java/com/ana/dev/githublistapp/presentation/main/MainActivity.kt
+++ app/src/main/java/com/ana/dev/githublistapp/presentation/main/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ana.dev.githublistapp.R
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
